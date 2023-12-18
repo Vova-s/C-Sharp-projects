41a38cd9333247923266d82832d8d7606c381699
@vova_s <92760957+Vova-s@users.noreply.github.com>
2023-12-18 12:12:20 +0100

    Add files via upload

---
+++ DijkstraShortestPath/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+
+namespace DijkstraShortestPath
+{
+    class Program
+    {
+        static int numberOfPoints;
+
+        static double[,] distanceToPoints = new double[5, 5] { { 0, 6, 0, 0, 11 }, { 6, 0, 8, 0, 0 }, { 0, 8, 0, 12, 7 }, { 0, 0, 12, 0, 4 }, { 11, 0, 7, 4, 0 } };
+        static void Main(string[] args)
+        {
+            OutputEncoding = System.Text.Encoding.UTF8;
+
+            numberOfPoints = 5;
+
+            Write("Start point: ");
+            int startPoint = Convert.ToInt32(Console.ReadLine()) - 1;
+            FillGraph();
+            DisplayGraph();
+            double[] distances = Dijkstra(startPoint);
+            DisplayDistances(startPoint, distances);
+        }
+
+        private static void FillGraph()
+        {
+            for (int i = 0; i < distanceToPoints.GetLength(0); i++)
+            {
+                for (int j = i; j < distanceToPoints.GetLength(0); j++)
+                {
+                    if (distanceToPoints[i, j] == 0)
+                    {
+                        distanceToPoints[i, j] = double.PositiveInfinity;
+                    }
+                    distanceToPoints[j, i] = distanceToPoints[i, j];
+                }
+            }
+        }
+        private static void DisplayGraph()
+        {
+            WriteLine("------------------------------------- My Graph -------------------------------------");
+            for (int i = 0; i < distanceToPoints.GetLength(0); i++)
+            {
+                for (int j = 0; j < distanceToPoints.GetLength(0); j++)
+                {
+                    Write($"{distanceToPoints[j, i],-3}");
+                }
+                WriteLine();
+            }
+        }
+        private static double[] Dijkstra(int startPoint)
+        {
+            double[] dist = new double[numberOfPoints];
+            List<int> s = new List<int>();
+            for (int j = 0; j < numberOfPoints; j++)
+            {
+                dist[j] = distanceToPoints[startPoint, j];
+                s.Add(j);
+            }
+            s.Remove(startPoint);
+            for (int i = 0; i < numberOfPoints - 1; i++)
+            {
+                int minValueIndex = Array.IndexOf(dist, dist.Where(j => s.Contains(Array.IndexOf(dist, j))).DefaultIfEmpty(int.MinValue).Min());
+                s.Remove(minValueIndex);
+
+                for (int j = 0; j < s.Count; j++)
+                {
+                    dist[s[j]] = Math.Min(dist[s[j]], dist[minValueIndex] + distanceToPoints[minValueIndex, s[j]]);
+                }
+            }
+            return dist;
+        }
+        private static void DisplayDistances(int startPoint, double[] distances)
+        {
+            for (int i = 0; i < distances.Length; i++)
+            {
+                WriteLine($"Your distance {startPoint + 1} to {i + 1} is {distances[i]}");
+            }
+        }
+    }
+}
---
+++ DijkstraShortestPathAlgorithm/Program.cs
@@ -0,0 +1,168 @@
+﻿namespace DijkstraShortestPathAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Matrix Data
+
+            var filePath = @".\matrix.txt"; // Path
+            var matrixData = File.ReadAllLines(filePath); // Read all lines from the file
+            var adjacencyMatrix = new int[matrixData.Length, matrixData[0].Split(' ').Length]; // Initialize a two-dimensional array
+            adjacencyMatrix = FillAdjacencyMatrix(matrixData, adjacencyMatrix); // Fill the adjacency matrix
+            var matrixSize = matrixData.Length; // Matrix size
+
+            #endregion
+
+            var shortestPathFinder = new DijkstraAlgorithm(adjacencyMatrix, matrixSize);
+
+            shortestPathFinder.PrintMatrix(); // Display the adjacency matrix
+
+            shortestPathFinder.FindShortestPaths(adjacencyMatrix, 0);
+        }
+
+        #region Fill Adjacency Matrix
+
+        static int[,] FillAdjacencyMatrix(string[] matrixData, int[,] adjacencyMatrix)
+        {
+            for (int i = 0; i < matrixData.Length; i++)
+            {
+                var rowValues = matrixData[i].Split(' ');
+                for (int j = 0; j < rowValues.Length; j++)
+                {
+                    try
+                    {
+                        adjacencyMatrix[i, j] = Convert.ToInt32(rowValues[j]);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("There are incorrect data types in your matrix");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            return adjacencyMatrix;
+        }
+
+        #endregion
+    }
+
+    public class DijkstraAlgorithm
+    {
+        public DijkstraAlgorithm(int[,] adjacencyMatrix, int matrixSize)
+        {
+            AdjacencyMatrix = adjacencyMatrix;
+            MatrixSize = matrixSize;
+        }
+
+        private int[,] AdjacencyMatrix { get; }
+        private int MatrixSize { get; }
+
+        public void PrintMatrix()
+        {
+            Console.WriteLine("Your adjacency matrix: ");
+            Console.WriteLine();
+            for (int i = 0; i < AdjacencyMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < AdjacencyMatrix.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}", AdjacencyMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private int MinDistance(int[] distances, bool[] shortestPathSet)
+        {
+            var min = int.MaxValue;
+            var minIndex = -1;
+
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                if (shortestPathSet[i] == false && distances[i] <= min)
+                {
+                    min = distances[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        public void FindShortestPaths(int[,] adjacencyMatrix, int startNode)
+        {
+            var distances = new int[MatrixSize];
+            var paths = new int[MatrixSize];
+            var shortestPathSet = new bool[MatrixSize];
+
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                distances[i] = int.MaxValue;
+                shortestPathSet[i] = false;
+            }
+
+            distances[startNode] = 0;
+
+            for (int i = 0; i < MatrixSize - 1; i++)
+            {
+                var minDistanceNode = MinDistance(distances, shortestPathSet);
+
+                shortestPathSet[minDistanceNode] = true;
+
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    if (!shortestPathSet[j] && adjacencyMatrix[minDistanceNode, j] != 0 &&
+                        distances[minDistanceNode] != int.MaxValue &&
+                        distances[minDistanceNode] + adjacencyMatrix[minDistanceNode, j] < distances[j])
+                    {
+                        distances[j] = distances[minDistanceNode] + adjacencyMatrix[minDistanceNode, j];
+                        paths[j] = minDistanceNode;
+                    }
+                }
+            }
+
+            Console.WriteLine("Path data (Dijkstra's algorithm):");
+            Console.WriteLine();
+            Console.WriteLine($"Starting node: 1");
+
+            for (int i = 1; i < MatrixSize; i++)
+            {
+                if (paths[i] == 0)
+                    Console.WriteLine($"Shortest path: from 1 -> {i + 1} direct | Minimum distance: {distances[i]}");
+                else
+                {
+                    var stack = new Stack<int>();
+                    stack.Push(paths[i] + 1);
+
+                    Console.Write($"Shortest path: from 1 -> ");
+
+                    for (int j = paths[i]; j != 0; j = paths[j])
+                    {
+                        if (paths[j] == 0)
+                            break;
+                        else
+                        {
+                            stack.Push(paths[j]);
+                            j = paths[j];
+                        }
+                    }
+
+                    for (int j = 0; j <= stack.Count; j++)
+                    {
+                        if (j == stack.Count)
+                            Console.Write($"{i + 1} | Minimum distance: {distances[i]}");
+                        else
+                        {
+                            Console.Write(stack.Pop() + " -> ");
+                            j = -1;
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
