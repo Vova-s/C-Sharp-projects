0e60c2c66a183d607c97fe2a54021a53551583c7
@vova_s <92760957+Vova-s@users.noreply.github.com>
2023-12-18 12:06:44 +0100

    Add files via upload

---
+++ CezarCipherProgram/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using static System.Console;
+
+namespace CezarCipherProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Write("input text to encode : "); string text = ReadLine().ToLower();
+            int key = 6;
+            int value = 2;
+            string enc = encryptingCezar(text, key, value);
+            WriteLine("encrypted text is : " + enc);
+            string decoded = decryptingCezar(enc, key);
+            WriteLine("decrypted text is : " + decoded);
+        }
+        public static string encryptingCezar(string text, int key, int value)
+        {
+            string encodedtext = " ";
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string alphabet1 = alphabet + alphabet.ToLower();
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                char letter = text[i];
+                int index = alphabet1.IndexOf(letter);
+                if (index < 0)
+                {
+                    encodedtext += letter.ToString();
+                }
+                int codeIndex = (index + key) % alphabet1.Length;
+                encodedtext += alphabet1[codeIndex];
+            }
+            for (int i = 1; i < text.Length; i += 2)
+            {
+                char letter = text[i];
+                int index = alphabet1.IndexOf(letter);
+                if (index < 0)
+                {
+                    encodedtext += letter.ToString();
+                }
+                int codeIndex = (index - value) % alphabet1.Length;
+                encodedtext += alphabet1[codeIndex];
+            }
+            return encodedtext;
+
+        }
+        public static string decryptingCezar(string encodedtext, int key)
+        {
+            string decodedtext = " ";
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string alphabet1 = alphabet + alphabet.ToLower();
+            for (int i = 0; i < encodedtext.Length; i++)
+            {
+                char letter = encodedtext[i];
+                int index = alphabet1.IndexOf(letter);
+                if (index < 0)
+                {
+
+                    decodedtext += letter.ToString();
+                }
+                else
+                {
+                    int codeIndex = (alphabet1.Length + index - key) % alphabet1.Length;
+                    decodedtext += alphabet1[codeIndex];
+                }
+            }
+            return decodedtext;
+
+
+        }
+
+    }
+}
---
+++ CleaningEquipmentFactorySystemByFactoryPattern/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using static System.Console;
+
+namespace CleaningEquipmentFactorySystemByFactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Cleaning factory: ");
+            CleaningFactory factory = new DryCleaningFactory();
+            factory.createManualCleaningHoover();
+            factory = new DryandWetCleaningFactory();
+            factory.createPortableCarHoover();
+
+        }
+    }
+    public class PortableCarHoover
+    {
+    }
+    public class ManualCleaningHoover
+    {
+    }
+    public class RobotHoover
+    {
+    }
+    public interface CleaningFactory
+    {
+        ManualCleaningHoover createManualCleaningHoover();
+        PortableCarHoover createPortableCarHoover();
+        RobotHoover createRobotHoover();
+    }
+    public class DryCleaningFactory : CleaningFactory
+    {
+        public DryCleaningFactory()
+        {
+            Console.WriteLine("\nProduction line has been changed to dry cleaning.");
+
+        }
+        public ManualCleaningHoover createManualCleaningHoover()
+        {
+            return new DryCleaningManualHoover();
+        }
+        public PortableCarHoover createPortableCarHoover()
+        {
+            return new DryCleaningPortableCarHoover();
+        }
+        public RobotHoover createRobotHoover()
+        {
+            return new DryCleaningRobotHoover();
+        }
+    }
+    public class DryandWetCleaningFactory : CleaningFactory
+    {
+        public DryandWetCleaningFactory()
+        {
+            Console.WriteLine("\nProduction line has been changed to dry and wet cleaning.");
+
+        }
+        public ManualCleaningHoover createManualCleaningHoover()
+        {
+            return new DryWetCleaningManualHoover();
+        }
+        public PortableCarHoover createPortableCarHoover()
+        {
+            return new DryCleaningPortableCarHoover();
+        }
+        public RobotHoover createRobotHoover()
+        {
+            return new DryWetCleaningHoover();
+        }
+    }
+    public class DryCleaningManualHoover : ManualCleaningHoover
+    {
+        public DryCleaningManualHoover()
+        {
+            Console.WriteLine("Manual cleaning hoover for dry cleaning has been created.");
+
+        }
+    }
+    public class DryCleaningPortableCarHoover : PortableCarHoover
+    {
+        public DryCleaningPortableCarHoover()
+        {
+            Console.WriteLine("Portable car hoover for dry cleaning has been created.");
+
+        }
+    }
+    public class DryCleaningRobotHoover : RobotHoover
+    {
+        public DryCleaningRobotHoover()
+        {
+            Console.WriteLine("Robot hoover for dry cleaning has been created.");
+        }
+    }
+    public class DryWetCleaningManualHoover : ManualCleaningHoover
+    {
+        public DryWetCleaningManualHoover()
+
+        {
+            Console.WriteLine("Manual cleaning hoover for dry and wet cleaning has been created.");
+
+        }
+    }
+    public class DryWetCleaningPortableCarHoover : PortableCarHoover
+    {
+        public DryWetCleaningPortableCarHoover()
+        {
+            Console.WriteLine("Portable car hoover for dry and wet cleaning has been created.");
+
+        }
+    }
+    public class DryWetCleaningHoover : RobotHoover
+    {
+        public DryWetCleaningHoover()
+        {
+            Console.WriteLine("Robot hoover for dry and wet  cleaning has been created.");
+
+        }
+    }
+}
